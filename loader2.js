d5fa93a22272b106b32e9cec7411fef59aa45c38
kais <kaissaroj@gmail.com>
2021-02-07 03:50:34 -0700

    Update and rename loader.js to loader2.js

---
+++ loader2.js
@@ -0,0 +1,44 @@
+function BMWidget() {
+  this.widgetDom = null;
+  this.dataSets = {};
+  this.url = "http://localhost:3000/bmapi/v1/widget/loader";
+}
+BMWidget.prototype.init = function () {
+  this.widgetDom = document.getElementById("bookmundi_widget_container");
+  this.dataSets = this.widgetDom.dataset;
+  this.dataSets = Object.assign({}, this.dataSets);
+  this.createRequest();
+};
+BMWidget.prototype.createRequest = function () {
+  const url = "https://ghcdn.rawgit.org/kaissaroj/testwidget/main/bm2.js";
+  loadJS(url);
+  // this.getRequest(function (status) {
+  //   console.log("request status", status);
+  // });
+};
+BMWidget.prototype.getRequest = function (callback) {
+  var http = new XMLHttpRequest();
+  http.open("POST", this.url, true);
+  http.setRequestHeader("Content-type", "application/json");
+  http.onload = function () {
+    callback(this.responseText);
+  };
+  http.send(JSON.stringify(this.dataSets));
+};
+var bmWidget = new BMWidget();
+function loadJS(src) {
+  var script = document.createElement("script");
+  script.onload = function () {};
+  script.src = src;
+  document.head.appendChild(script);
+}
+
+function injectCss(css) {
+  var link = document.createElement("link");
+  link.href = "https://ghcdn.rawgit.org/kaissaroj/testwidget/main/bm.css";
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+export default bmWidget;
