a1de45ed9be937983dda70d509aad510f7a4a33c
kais <kaissaroj@gmail.com>
2021-02-12 13:55:18 +0545

    Update and rename loaderv1.5.js to loaderv1.6.js

--- loaderv1.6.js
+++ loaderv1.6.js
@@ -9,7 +9,7 @@ BMWidget.prototype.init = function () {
   this.createRequest();
 };
 BMWidget.prototype.createRequest = function () {
-  const url = "https://cdn.jsdelivr.net/gh/kaissaroj/testwidget@raw/main/bmv1.5.js";
+  const url = "https://cdn.jsdelivr.net/gh/kaissaroj/testwidget@raw/main/bmv1.6.js";
   injectCss()
   loadJS(url);
 };
