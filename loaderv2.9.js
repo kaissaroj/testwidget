2dc31054bb5eca0ca1054ae87f20b81db8df6e93
kais <kaissaroj@gmail.com>
2021-02-24 18:42:12 +0545

    Update and rename loaderv2.8.js to loaderv2.9.js

--- loaderv2.9.js
+++ loaderv2.9.js
@@ -9,7 +9,7 @@ BMWidget.prototype.init = function () {
   this.createRequest();
 };
 BMWidget.prototype.createRequest = function () {
-  const url = "https://cdn.jsdelivr.net/gh/kaissaroj/testwidget@main/bmv2.8.js";
+  const url = "https://cdn.jsdelivr.net/gh/kaissaroj/testwidget@main/bmv2.9.js";
   injectCss()
   loadJS(url);
 };
